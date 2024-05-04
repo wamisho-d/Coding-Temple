6de7854a6c1105f1fb26bb4571ac081064f1e79d
wamisho_d <wamisho.bogale1989@gmail.com>
2024-05-03 22:33:05 -0400

    first commit

---
+++ All.py
@@ -0,0 +1,78 @@
+# Question 1 Task 1: Code Correction 
+weather = "sunny"
+if weather == "sunny":
+    print("wear sunglasses!")
+else:
+    print("Take an umberella!")
+
+# Question 1 Task 2: Your Mood Today
+Mood = input("How are you feeling today?")
+if Mood == "happy":
+    print("That's great to hear!")
+if Mood == "sad":
+    print("I hope your day gets better!")
+
+# Question 2 Task 1: Code Correction
+PI_VAlUE = 3.14
+user_age = 25
+user_location= "New York"
+MAX_LIMIT = 1000
+
+# Question 3 Task 1: Code Correction 
+variable_a = "Hello, World"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
+
+# Question 4 Task 1: Grocery Store Math
+# cost of bread $8, cost of butter $4, cost of jelly $2
+bread = 8
+peanut_butter = 4
+jelly = 2
+# calculate total cost of three items: 5 bread, 2 peanut butter, 4 jelly
+total_cost = (5*8) + (2*4) + (4*2)
+print(total_cost)
+
+# Question 4 Task 2: Bank interest
+# intial_amount = 10000
+# interest_rate = %7
+initial_amount = 10000
+interest_rate = 7
+total_amount = initial_amount*(1+7/100)
+print(total_amount)
+
+# Question 5 Task 1:
+# swapping two variables
+a = 32
+b = 47
+# swapping two variables
+# swapping using multiplication and division oprator
+a = a*b
+b = a/b
+a = a/b
+print("Value of a : ", a)
+print("Value of b : ", b)
+
+# initial values
+x = 29
+y = 30
+print("initial values:x=",x,",y =",y)
+# swapping values
+temp = x
+x = y
+y = temp 
+print("Swapped values:x=",x,",y =",y)
+
+# Question 6 Task 1: Validating Calculations 
+x = 5 + 2 * 9
+y = (5 + 2) * 9 
+print(x)
+print(y)
+
+# Question 6 Task 2: Mix and Match
+x = (6 /3 < 8) and (10/2 < 12)
+print(x)
